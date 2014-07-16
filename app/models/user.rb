19f21711eb9f4467559bbe98f542998829555a96
Celeen Rusk & Colin Trent <pair+celeen.trentcs@devbootcamp.com>
2014-07-16 16:09:26 -0500

    models and migrations up, tested in console

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :reviews, foreign_key: "reviewer_id"
+  has_many :points, through: :reviews, source: :votes
+  has_many :comments, foreign_key: "commenter_id"
+  has_many :restaurants, foreign_key: "creator_id"
+  has_many :votes_cast, class_name: "Vote", foreign_key: "voter_id"
 end
