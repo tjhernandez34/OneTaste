a3230447cd9c693f5ef0a1417fc5c174f214b62e
Celeen Rusk & Colin Trent <pair+celeen.trentcs@devbootcamp.com>
2014-07-17 13:32:07 -0500

    many restaurant model tests written

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,7 @@ class User < ActiveRecord::Base
   has_many :restaurants, foreign_key: "creator_id"
   has_many :votes_cast, class_name: "Vote", foreign_key: "voter_id"
 
-  validates_presence_of :username, :email, :password_digest
+  validates_presence_of :username, :email, :password
   validates_uniqueness_of :username, :email
   validates :password, :length => {:minimum => 6, :message => "Password must be at least 6 characters"}
-
-
 end
