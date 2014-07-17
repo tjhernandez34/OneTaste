ccc612a402896fe2c18b5efdd663e617302790dd
Celeen Rusk & Colin Trent <pair+celeen.trentcs@devbootcamp.com>
2014-07-17 10:16:46 -0500

    Travis with test db

---
+++ spec/controllers/users_spec.rb
@@ -0,0 +1,14 @@
+require 'spec helper'
+
+describe UsersController do
+  context "sign up" do
+    it "should successfully load the new user page" do
+      get :new
+      expect(response.status).to eq 200
+    end
+
+    it "should create a new user" do
+      get :create {  }
+    end
+  end
+end
