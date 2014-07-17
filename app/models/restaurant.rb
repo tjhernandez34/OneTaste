65858b6fae9e3b7f76f4522eff5a05357a077dc9
Celeen Rusk & Colin Trent <pair+celeen.trentcs@devbootcamp.com>
2014-07-17 10:58:38 -0500

    all greens for most models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,7 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews
   has_many :votes
   belongs_to :creator, class_name: "User"
+
+  validates_uniqueness_of :street_address
+  validates_presence_of :name, :state, :city
 end
