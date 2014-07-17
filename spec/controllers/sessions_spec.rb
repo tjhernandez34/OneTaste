0fb12c361753d6848a1a242a8e12f425c508dff4
Celeen Rusk & Colin Trent <pair+celeen.trentcs@devbootcamp.com>
2014-07-17 17:04:08 -0500

    lots more model tests

--- spec/controllers/sessions_spec.rb
+++ spec/controllers/sessions_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 
 describe SessionsController do
+
   context "login page" do
     it "should be return success status" do
       get :new
       expect(response.status).to eq(200)
+    end
+  end
+
+  context "#create" do
+    it "should redirect to index if user is authenticated" do
+
+    end
+
+    it "should redirect to /login if user is not authenticated" do
     end
   end
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -11,7 +11,7 @@ describe Restaurant do
   context "associations" do
     it { should have_many :reviews }
     it { should have_many :votes }
-    it { should belong_to :creator }
+    it { should belong_to(:creator).class_name("User") }
   end
 
   context "#self.search" do
@@ -38,4 +38,29 @@ describe Restaurant do
       expect(Restaurant.search("vegetarian").first.name).to eq("Bernie's Bistro")
     end
   end
+
+  let(:restaurant) {Restaurant.create(name: "Bernie's Bistro", creator_id: 5, category: "Vegetarian", street_address: "125 Fake Street", city: "Gotham", state: "NY")
+}
+
+  it "should have a name" do
+    expect(restaurant.name).to eq("Bernie's Bistro")
+  end
+
+  it "should have a creator_id" do
+    expect(restaurant.creator_id).to eq(5)
+  end
+
+  it "should have a street_address" do
+    expect(restaurant.street_address).to eq("125 Fake Street")
+  end
+
+  it "should have a city and state" do
+    expect(restaurant.city).to eq("Gotham")
+    expect(restaurant.state).to eq("NY")
+  end
+
+  it "should have a category" do
+    expect(restaurant.category).to eq("Vegetarian")
+  end
+
 end
