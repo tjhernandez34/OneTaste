9c55722546cddf6703b63213da142a764f84c21c
Celeen Rusk & Colin Trent <pair+celeen.trentcs@devbootcamp.com>
2014-07-17 10:33:46 -0500

    passing user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,14 @@ describe User do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
-    it { should validate_length_of(:password) }
+    it { should ensure_length_of(:password) }
+  end
+
+  context "associations" do
+    it { should have_many :reviews}
+    it { should have_many :points}
+    it { should have_many :comments}
+    it { should have_many :restaurants }
+    it { should have_many :votes_cast }
   end
 end
