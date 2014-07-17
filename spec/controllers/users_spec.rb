dc8abcfc34bf65748d2571f2dda680f64af6477b
Celeen Rusk & Colin Trent <pair+celeen.trentcs@devbootcamp.com>
2014-07-17 14:49:26 -0500

    Tests and ico

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,14 +1,19 @@
 require 'spec_helper'
 
 describe UsersController do
-  context "sign up" do
     it "should successfully load the new user page" do
-      get :new
+      get :index
       expect(response.status).to eq 200
     end
 
     it "should create a new user" do
-      # get :create {  }
+      post :create, user: {username: "hans", email: "han@solo.com", password: "password" }
+      expect(User.last.username).to eq("hans")
     end
-  end
+
+    it "should render the user info on a page" do
+      user = User.create(username: "hans", email: "han@solo.com", password: "password")
+      visit user_url(user)
+      expect(response.status).to eq(200)
+    end
 end
