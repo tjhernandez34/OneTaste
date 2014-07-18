f8ad9b822fc345b34c112ab980bc66f62ce781f5
Celeen Rusk & Colin Trent <pair+celeen.trentcs@devbootcamp.com>
2014-07-17 20:38:03 -0500

    some more controller test

--- spec/controllers/restaurants_spec.rb
+++ spec/controllers/restaurants_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
 
 describe RestaurantsController do
+  context "#index" do
+    it "should collect all restaurants" do
+      get :index
+      assigns(:restaurants).should == Restaurant.all
+    end
+  end
 
+  context "#search" do
+    it "should collect search results" do
+     restaurant =  Restaurant.create(name: "Bernie's Bistro", creator_id: 5, category: "Vegetarian", street_address: "125 Fake Street", city: "Gotham", state: "NY")
+      post :search, search: "Bernie's Bistro"
+      (assigns(:results)[0].name).should eq("Bernie's Bistro")
+    end
+  end
 
+  context '#new' do
+    it "should create a new restaurant object" do
+      get :new
+      assigns(:restaurant).should be_a(Restaurant)
+    end
+  end
 
+  context '#create' do
+    xit "should create a restaurant object" do
+      post :create, restaurant: {name: "Bernie's Bistro", creator_id: 5, category: "Vegetarian", street_address: "125 Fake Street", city: "Gotham", state: "NY"}
+      # should redirect_to(restaurant_path(3))
+    end
+  end
 end
