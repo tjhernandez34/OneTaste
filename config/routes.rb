42c6fe3c8f31be9b1bbcf1e9143eec7430b0213b
Travis Hernandez & Colin Trent <pair+tjhernandez34.trentcs@devbootcamp.com>
2014-07-19 16:20:29 -0500

    started friend funtionality, can display friends

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
     resources :votes, :only => :create
   end
 
+  resources :friendships
+
   resources :comments do
     resources :votes, :only => :create
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718202822) do
+ActiveRecord::Schema.define(version: 20140719210627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20140718202822) do
     t.integer  "parent_id"
     t.text     "content"
     t.integer  "commenter_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -58,6 +65,7 @@ ActiveRecord::Schema.define(version: 20140718202822) do
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
+    t.integer  "friend_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "avatar"
