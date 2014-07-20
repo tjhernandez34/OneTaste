fbbe3e982edbe96ddb5dbef336180a2a57883ae4
Travis Hernandez & Colin Trent <pair+tjhernandez34.trentcs@devbootcamp.com>
2014-07-20 18:00:14 -0500

    fix js vote function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,34 @@
+ $(document).ready(function() {
+ $(".vote").on('click', function(event) {
+        event.preventDefault();
+        that = $(this).parent();
+        console.log($(this).data('url'));
+        data = ($(this).data("type"));
+        id = ($(this).data("id")).split(" ")[1]
+        console.log(id)
+        label = ($(this).data("id")).split(" ")[0]
+        console.log(label)
+        user_id = ($(this).data("user"))
+        console.log(user_id)
+        $.ajax({
+            url: $(this).data('url'),
+            type: "Post",
+            data: {
+                vote: {
+                    voteable_type: data,
+                    voteable_id: id,
+                    voter_id: user_id
+                }
+            },
+            dataType: "json",
+            success: function(response) {
+                $("#" + label + "_" + id).text(response + " YUMZ!");
+                console.log("#" + label + "_" + id)
+                console.log(response);
+            }
+        });
+    })
+})
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
