e1cf6ce150e063b728bc71ea2d324341f8ef58ac
celeen <celeenrusk@gmail.com>
2014-07-20 12:15:52 -0500

    trying to make this modal stuff work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,104 +1,122 @@
 var currentImage = 1;
-  function showImage() {
-      array_length = $('#images_length').val()
-      // console.log("in loop")
-      // console.log(array_length)
 
-      last = "#restimage" + (currentImage - 1);
-      string = "#restimage" + currentImage;
-      // console.log(last)
-      // console.log(string)
-      // console.log(currentImage)
-      $(last).removeClass('restaurant_image_visible');
-      $(last).addClass('restaurant_image_hidden');
-      $(string).removeClass('restaurant_image_hidden');
-      $(string).addClass('restaurant_image_visible');
+function showImage() {
+    array_length = $('#images_length').val()
+    // console.log("in loop")
+    // console.log(array_length)
 
- };
+    last = "#restimage" + (currentImage - 1);
+    string = "#restimage" + currentImage;
+    // console.log(last)
+    // console.log(string)
+    // console.log(currentImage)
+    $(last).removeClass('restaurant_image_visible');
+    $(last).addClass('restaurant_image_hidden');
+    $(string).removeClass('restaurant_image_hidden');
+    $(string).addClass('restaurant_image_visible');
+
+};
 
 
-$(document).ready(function(){
-  $(".vote").on('click', function(event){
-    event.preventDefault();
-    that = $(this).parent();
-    console.log($(this).data('url'));
-    data = ($(this).data("type"));
-    id = ($(this).data("id")).split(" ")[1]
-    console.log(id)
-    label = ($(this).data("id")).split(" ")[0]
-    console.log(label)
-    user_id = ($(this).data("user"))
-    console.log(user_id)
-    $.ajax({
-      url:$(this).data('url'),
-      type: "Post",
-      data: {vote: {voteable_type: data, voteable_id: id, voter_id: user_id}},
-      dataType: "json",
-      success: function(response){
-        $("#" + label + "_" + id).text(response);
-        console.log("#" + label + "_" + id)
-        console.log(response);
-      }
+$(document).ready(function() {
+    $(".vote").on('click', function(event) {
+        event.preventDefault();
+        that = $(this).parent();
+        console.log($(this).data('url'));
+        data = ($(this).data("type"));
+        id = ($(this).data("id")).split(" ")[1]
+        console.log(id)
+        label = ($(this).data("id")).split(" ")[0]
+        console.log(label)
+        user_id = ($(this).data("user"))
+        console.log(user_id)
+        $.ajax({
+            url: $(this).data('url'),
+            type: "Post",
+            data: {
+                vote: {
+                    voteable_type: data,
+                    voteable_id: id,
+                    voter_id: user_id
+                }
+            },
+            dataType: "json",
+            success: function(response) {
+                $("#" + label + "_" + id).text(response);
+                console.log("#" + label + "_" + id)
+                console.log(response);
+            }
+        });
+    })
+
+    $(".pure-input-1-4").on('click', function() {
+        $("#slogan").fadeTo("slow", 0.4);
+        $('#search_tag').hide();
+        $("#box").attr('class', 'pure-input-1-2');
+
     });
-  })
 
-  $(".pure-input-1-4").on('click', function(){
-    $("#slogan").fadeTo( "slow" , 0.4);
-    $('#search_tag').hide();
-    $("#box").attr('class', 'pure-input-1-2');
+    $("#slogan").on('click', function() {
+        $("#slogan").fadeTo("slow", 1);
+        $("#box").attr('class', 'pure-input-1-4');
 
-  });
+    });
 
-  $("#slogan").on('click', function(){
-    $("#slogan").fadeTo( "slow" , 1);
-    $("#box").attr('class', 'pure-input-1-4');
+    $(document).scroll(function() {
+        if ($(document).scrollTop() > 30) {
+            $(".restaurants_splash").show();
+            $("#users_splash").show();
+            $("#slogan").css('height', '550px');
+        }
+        if ($(document).scrollTop() == 0) {
+            $("#slogan").css('height', '900px');
+            $(".restaurants_splash").hide();
+            $("#users_splash").hide();
+        };
+    });
 
-  });
+    $(".restaurants_splash").on('click', function() {
+        $("#slogan").css('opacity', '1');
+        $("#box").attr('class', 'pure-input-1-4');
+    });
 
-  $(document).scroll(function(){
-    if($(document).scrollTop() > 30){
-      $(".restaurants_splash").show();
-      $("#users_splash").show();
-      $("#slogan").css('height', '550px');}
-    if($(document).scrollTop() == 0){
-      $("#slogan").css('height', '900px');
-      $(".restaurants_splash").hide();
-      $("#users_splash").hide();
-    };
-  });
+    $("#users_splash").on('click', function() {
+        $("#slogan").css('opacity', '1');
+        $("#box").attr('class', 'pure-input-1-4');
+    });
 
-  $(".restaurants_splash").on('click', function(){
-    $("#slogan").css('opacity', '1');
-    $("#box").attr('class', 'pure-input-1-4');
-  });
-
-  $("#users_splash").on('click', function(){
-    $("#slogan").css('opacity', '1');
-    $("#box").attr('class', 'pure-input-1-4');
-  });
-
-setInterval( function(){showImage()
-  currentImage++;
-  // console.log("just outside if");
-  // console.log(currentImage);
-  // console.log(array_length);
-  // console.log(currentImage === array_length);
-  if (currentImage > array_length ) {
-      // console.log("if statement");
-      $(last).removeClass('restaurant_image_visible');
-      $(last).addClass('restaurant_image_hidden');
-      $('#restimage0').removeClass('restaurant_image_hidden');
-      $('#restimage0').addClass('restaurant_image_visible');
-      currentImage = 0;
-  };
-}, 5000);
+    setInterval(function() {
+        showImage()
+        currentImage++;
+        // console.log("just outside if");
+        // console.log(currentImage);
+        // console.log(array_length);
+        // console.log(currentImage === array_length);
+        if (currentImage > array_length) {
+            // console.log("if statement");
+            $(last).removeClass('restaurant_image_visible');
+            $(last).addClass('restaurant_image_hidden');
+            $('#restimage0').removeClass('restaurant_image_hidden');
+            $('#restimage0').addClass('restaurant_image_visible');
+            currentImage = 0;
+        };
+    }, 5000);
 
 })
 
+//---------------Modals JS------------
+$('.login').on('click', function(event) {
+    event.preventDefault();
+    $('.login-form').fadeToggle(400);
+});
+
+$('.signup').on('click', function(event) {
+    event.preventDefault();
+    $('.signup-form-modal').fadeToggle(400);
+});
 
 
-
-//   });
+// });
 
 
 
