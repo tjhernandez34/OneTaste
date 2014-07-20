515bb2d5f86b1b97bfb663e5427fed387c61f6df
celeen <celeenrusk@gmail.com>
2014-07-19 20:33:13 -0500

    modals, 80%

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,38 +1,56 @@
-$(document).ready(function(){
-  $(".vote").on('click', function(event){
-    event.preventDefault();
-    that = $(this).parent();
-    console.log($(this).data('url'));
-    data = ($(this).data("type"));
-    id = ($(this).data("id")).split(" ")[1]
-    console.log(id)
-    label = ($(this).data("id")).split(" ")[0]
-    console.log(label)
-    user_id = ($(this).data("user"))
-    console.log(user_id)
-    $.ajax({
-      url:$(this).data('url'),
-      type: "Post",
-      data: {vote: {voteable_type: data, voteable_id: id, voter_id: user_id}},
-      dataType: "json",
-      success: function(response){
-        $("#" + label + "_" + id).text(response);
-        console.log("#" + label + "_" + id)
-        console.log(response);
-      }
- });
-})
+$(document).ready(function() {
+    $(".vote").on('click', function(event) {
+        event.preventDefault();
+        that = $(this).parent();
+        console.log($(this).data('url'));
+        data = ($(this).data("type"));
+        id = ($(this).data("id")).split(" ")[1]
+        console.log(id)
+        label = ($(this).data("id")).split(" ")[0]
+        console.log(label)
+        user_id = ($(this).data("user"))
+        console.log(user_id)
+        $.ajax({
+            url: $(this).data('url'),
+            type: "Post",
+            data: {
+                vote: {
+                    voteable_type: data,
+                    voteable_id: id,
+                    voter_id: user_id
+                }
+            },
+            dataType: "json",
+            success: function(response) {
+                $("#" + label + "_" + id).text(response);
+                console.log("#" + label + "_" + id)
+                console.log(response);
+            }
+        });
+    })
 
-  $(".pure-input-1-4").on('click', function(){
-    $("#slogan").fadeTo( "slow" , 0.2);
+    $(".pure-input-1-4").on('click', function() {
+        $("#slogan").fadeTo("slow", 0.2);
 
-  });
+    });
+
+    //------------ Login/Signup Modals JS-----------------
 
 
+    $('.login').on('click', function(event) {
+        event.preventDefault();
+        $('.login-form').fadeToggle(400);
+    })
 
-  // $("#slogan").on('click', function(){
-  //   $("#slogan").css('opacity', '1');
-})
+    $('.signup').on('click', function(event) {
+        event.preventDefault();
+        $('.signup-form-modal').fadeToggle(400);
+    })
+
+
+    // $("#slogan").on('click', function(){
+    //   $("#slogan").css('opacity', '1');
+});
 
 
 
@@ -53,6 +71,7 @@ $(document).ready(function(){
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require sessions
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
