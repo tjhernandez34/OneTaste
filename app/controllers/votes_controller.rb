a5c4a3add7c55d2c562158689a34a8fe46a6ce9e
Travis Hernandez & Colin Trent <pair+tjhernandez34.trentcs@devbootcamp.com>
2014-07-20 19:13:53 -0500

    yayayayayaya

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,15 +2,17 @@ class VotesController < ApplicationController
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json'}
 
   def create
+    puts params
+    puts params[:vote]
     previous_vote = Vote.where('voter_id = ? AND voteable_id = ? AND voteable_type = ?', params[:vote][:voter_id], params[:vote][:voteable_id], params[:vote][:voteable_type])
     if previous_vote.length > 0
       raise 'You already voted!'
     else
       Vote.create(vote_params)
     end
-
+    logger.info "after the if statement"
     new_number = Vote.where('voteable_id = ? AND voteable_type = ?', params[:vote][:voteable_id], params[:vote][:voteable_type]).count.to_s
-
+    puts 'NEW NUMBER #{new_number}'
     render json: new_number
   end
 
